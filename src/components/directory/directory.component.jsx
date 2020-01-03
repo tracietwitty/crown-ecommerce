c821c382affc3d1d005d085bdab0cd3ddd360475
Tracie T <traciecodes@gmail.com>
2020-01-02 20:04:52 -0600

    WIP: Got menu items to render, started styling them

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import MenuItem from '../menu-item/menu-item.component';
 import './directory.style.scss';
+
 class Directory extends React.Component {
 	constructor() {
 		super();
@@ -46,8 +47,8 @@ class Directory extends React.Component {
 	render() {
 		return (
 			<div className='directory-menu'>
-				{this.state.sections.map(({ title, imageUrl, id }) => (
-					<MenuItem key={id} title={title} />
+				{this.state.sections.map(({ title, imageUrl, id, size }) => (
+					<MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
 				))}
 			</div>
 		);
