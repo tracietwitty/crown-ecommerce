4da634613487d7f2862a579182f4007385a0da41
Tracie T <traciecodes@gmail.com>
2020-01-04 16:55:35 -0600

    Added form component with basic sign-in input fields

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInSignUp from './pages/signin-signup/signin-signup.component';
 
 function App() {
 	return (
@@ -12,6 +13,7 @@ function App() {
 			<Switch>
 				<Route exact path='/' component={HomePage} />
 				<Route path='/shop' component={ShopPage} />
+				<Route path='/signin' component={SignInSignUp} />
 			</Switch>
 		</div>
 	);
