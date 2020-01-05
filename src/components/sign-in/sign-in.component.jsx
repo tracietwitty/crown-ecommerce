e75ef4a7fd99ebabff9f39ed173d71dfbbc6a373
Tracie T <traciecodes@gmail.com>
2020-01-05 10:35:39 -0600

    Corrected style file names to 'styles'. Started working on form input component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './sign-in.style.scss';
+import './sign-in.styles.scss';
 import { render } from '@testing-library/react';
 
 class SignIn extends React.Component {
