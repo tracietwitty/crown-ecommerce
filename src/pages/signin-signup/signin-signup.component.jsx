98d48b3af63f717167069768c5b5a2abc324762c
Tracie T <traciecodes@gmail.com>
2020-01-06 13:00:53 -0600

    Included ability to store auth users into firebase

--- src/pages/signin-signup/signin-signup.component.jsx
+++ src/pages/signin-signup/signin-signup.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
 import './signin-signup.styles.scss';
 
 const SignInSignUp = () => (
 	<div className='sign-in-and-sign-up'>
 		<SignIn />
+		<SignUp />
 	</div>
 );
 
