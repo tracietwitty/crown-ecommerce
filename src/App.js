ca5ac836ff95662199d5200d58a2100a9e70775b
Tracie T <traciecodes@gmail.com>
2020-01-13 13:17:00 -0600

    updating app to redirect to home on sign in

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
+
 import { connect } from 'react-redux';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
@@ -49,15 +50,25 @@ class App extends React.Component {
 				<Switch>
 					<Route exact path='/' component={HomePage} />
 					<Route path='/shop' component={ShopPage} />
-					<Route path='/signin' component={SignInSignUp} />
+					<Route
+						exact
+						path='/signin'
+						render={() =>
+							this.props.currentUser ? <Redirect to='/' /> : <SignInSignUp />
+						}
+					/>
 				</Switch>
 			</div>
 		);
 	}
 }
 
+const mapStateToProps = ({ user }) => ({
+	currentUser: user.currentUser
+});
+
 const mapDispatchToProps = dispatch => ({
 	setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,5 +1,7 @@
+import { UserActionTypes } from './user.types';
+
 export const setCurrentUser = user => ({
 	//action type must match what the reducer expects (see user.reducer.js)
-	type: 'SET_CURRENT_USER',
+	type: UserActionTypes.SET_CURRENT_USER,
 	payload: user
 });
