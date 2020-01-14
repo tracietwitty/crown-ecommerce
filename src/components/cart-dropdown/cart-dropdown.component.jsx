1a45adb98566ab4f54f6c3672d579b4a8330477e
Tracie T <traciecodes@gmail.com>
2020-01-14 16:06:41 -0600

    Added checkout item component, updated cart to use selectors

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,11 +3,12 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { withRouter } from 'react-router-dom';
 import { selectCartItems } from '../../redux/cart/cart.selectors';
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 import CartItem from '../cart-item/cart-item.component';
 import CustomButton from '../custom-button/custom-button.component';
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = ({ cartItems, history }) => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
 	<div className='cart-dropdown'>
 		<div className='cart-items'>
 			{cartItems.length ? (
@@ -18,7 +19,12 @@ const CartDropdown = ({ cartItems, history }) => (
 				<span className='empty-message'> Your cart is empty </span>
 			)}
 		</div>
-		<CustomButton onClick={() => history.push('/checkout')}>
+		<CustomButton
+			onClick={() => {
+				history.push('/checkout');
+				dispatch(toggleCartHidden);
+			}}
+		>
 			GO TO CHECKOUT
 		</CustomButton>
 	</div>
