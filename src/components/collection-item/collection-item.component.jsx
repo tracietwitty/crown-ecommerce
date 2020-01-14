0043e6df7b5d0a89dfac174882a8746aae5d0978
Tracie T <traciecodes@gmail.com>
2020-01-13 19:29:25 -0600

    Added styling for 'add to cart' button

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
-
+import { connect } from 'react-redux';
+import { addItem } from '../../redux/cart/cart.actions';
+import CustomButton from '../custom-button/custom-button.component';
 import './collection-item.styles.scss';
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
+const CollectionItem = ({ id, name, price, imageUrl, addItem }) => (
 	<div className='collection-item'>
 		<div
 			className='image'
@@ -14,7 +16,12 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
 			<span className='name'>{name}</span>
 			<span className='price'>{price}</span>
 		</div>
+		<CustomButton inverted> Add to cart </CustomButton>
 	</div>
 );
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+	addItem: item => dispatch(addItem(item))
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
