873d6ea758c04932fdd82fe848663024bc1771a0
Tracie T <traciecodes@gmail.com>
2020-01-20 18:33:27 -0600

    WIP: trying to finish moving each subcategory of items into their own components but #broken

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import CollectionOverview from '../../components/collection-overview/collection-overview.component';
-import { selectCollections } from '../../redux/shop/shop.selectors';
+import CollectionPage from '../collection/collection.component';
 
-const ShopPage = ({ collections }) => (
+const ShopPage = ({ match }) => (
 	<div className='shop-page'>
-		<CollectionOverview />
+		<Route exact path={`${match.path}`} component={CollectionOverview} />
+		<Route path={`${match.path}/:categoryId`} component={CollectionPage} />
 	</div>
 );
 
