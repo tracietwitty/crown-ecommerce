25b1d973bf6100f52cda388f560b4d9868f8cc9e
Tracie T <traciecodes@gmail.com>
2020-02-03 19:13:32 -0600

    Still working on switching over to styled components

--- src/components/collection-overview/collection-overview.component.jsx
+++ src/components/collection-overview/collection-overview.component.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-import './collection-overview.style.scss';
+import { CollectionsOverviewContainer } from './collection-overview.styles';
 import CollectionPreview from '../collection-preview/collection-preview.component';
 import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
 
 const CollectionOverview = ({ collections }) => (
-	<div className='collection-overview'>
+	<CollectionsOverviewContainer>
 		{collections.map(({ id, ...otherCollectionProps }) => (
 			<CollectionPreview key={id} {...otherCollectionProps} />
 		))}
-	</div>
+	</CollectionsOverviewContainer>
 );
 
 const mapStateToProps = createStructuredSelector({
