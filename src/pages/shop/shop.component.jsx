8ade013db6fbb8919cd845ef6254c0e0209a5084
Tracie T <traciecodes@gmail.com>
2020-02-05 19:35:01 -0600

    Corrected 'null error' issue and added a loading animation

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,17 +2,25 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-import CollectionOverview from '../../components/collection-overview/collection-overview.component';
-import CollectionPage from '../collection/collection.component';
-
 import {
 	firestore,
 	convertCollectionsSnapshotToMap
 } from '../../firebase/firebase.utils';
 
 import { updateCollections } from '../../redux/shop/shop.actions';
+import WithSpinner from '../../components/with-spinner/with-spinner.component';
+
+import CollectionsOverview from '../../components/collection-overview/collection-overview.component';
+import CollectionPage from '../collection/collection.component';
+
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 
 class ShopPage extends React.Component {
+	state = {
+		loading: true
+	};
+
 	unsubscribeFromSnapshot = null;
 
 	componentDidMount() {
@@ -28,12 +36,21 @@ class ShopPage extends React.Component {
 
 	render() {
 		const { match } = this.props;
+		const { loading } = this.state;
 		return (
 			<div className='shop-page'>
-				<Route exact path={`${match.path}`} component={CollectionOverview} />
+				<Route
+					exact
+					path={`${match.path}`}
+					render={props => (
+						<CollectionsOverviewWithSpinner isLoading={loading} {...props} />
+					)}
+				/>
 				<Route
 					path={`${match.path}/:collectionId`}
-					component={CollectionPage}
+					render={props => (
+						<CollectionPageWithSpinner isLoading={loading} {...props} />
+					)}
 				/>
 			</div>
 		);
